0ef0f84bea6dfca1b14d6934b4a970a051d2d588
David T Eady <61388628+DevTeady@users.noreply.github.com>
2020-08-01 15:32:02 +0600

    Update English.py

--- haruka/modules/translations/English.py
+++ haruka/modules/translations/English.py
@@ -201,13 +201,7 @@ This will create two buttons on a single line, instead of one button per line.
 EnglishStrings = {
     "send-start": """Hi {}, my name {}! If you have any questions on how to use me, read /help - and then head to @YanaBotGroup.
 
-I'm a group manager bot maintained by [this person](tg://user?id={654839744}). I'm a [Marie](https://github.com/PaulSonOfLars/tgbot) fork.
-I'm built in python3, using the \
-python-telegram-bot library, and am fully opensource - you can find what makes me tick\
-[here](https://github.com/peaktogoo/HarukaAya))!
-
-Feel free to submit pull requests on github, or to contact my support group, @EchelonTeam, with any bugs, questions \
-or feature requests you might have :)
+I'm a group manager bot maintained by [this person](tg://user?id={654839744}).
 
 If you enjoying using me and/or would like to help me survive in the wild, hit /donate to help fund/upgrade my VPS!
 """,
